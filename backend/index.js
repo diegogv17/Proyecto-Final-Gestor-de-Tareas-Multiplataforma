d33801710166c6a085ab7d848ad2eee0f00be7ad
Diego Gómez <rarodriguez@catedratico.uspg.edu.gt>
2026-02-18 19:17:32 -0600

    feat: agregar configuración básica del servidor con Express y dotenv

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import express from 'express'
+import route from './routes/userRoutes.js';
+//import dbClient from './config/dbClient.js';
+
+const app = express();
+
+
+
+app.use('/user', route)
+
+try{
+    const PORT = process.env.PORT || 300;
+    app.listen(PORT, () => console.log('servidor Activo en el puerto ' + PORT))
+}
+catch(e){
+ console.log(e)
+}
